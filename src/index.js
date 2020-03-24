06554c1572968b0d4f8ab819503e3774bb146e92
y-kawamura <tetora.y@gmail.com>
2020-03-24 11:29:23 +0900

    Set up echo bot

---
+++ src/index.js
@@ -0,0 +1,64 @@
+const line = require('@line/bot-sdk');
+const express = require('express');
+
+require('dotenv').config();
+
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET
+};
+
+// create LINE SDK client
+const client = new line.Client(config);
+
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent))
+    .then(result => res.json(result))
+    .catch(err => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+function notFound(req, res, next) {
+  res.status(404);
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  next(error);
+}
+
+function errorHandler(err, req, res, next) {
+  res.status(res.statusCode === 200 ? 500 : res.statusCode);
+  res.json({
+    message: err.message
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
